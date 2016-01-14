31a903157d8652c9d79d801665cdd47a5f300a3d
Marten <martenvoorberg@gmail.com>
2016-01-14 10:53:48 +0100

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
 Player::Player() {
+  playerSprite.loadFromFile("images/player.png");
+}
 
+void Player::moveUp() {
+ playerSprite.move(50, 0);
+ player.setTextureRect(playerDimension);
+ //add animation
 }
