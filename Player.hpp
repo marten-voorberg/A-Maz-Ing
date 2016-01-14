79bd8b2e572c60fd74a67db42578c939ac2634a8
Marten <martenvoorberg@gmail.com>
2016-01-14 10:29:42 +0100

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -1,1 +1,18 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 
+class Player {
+public:
+  void moveUp();
+  void moveDown();
+  void moveLeft();
+  void moveRight();
+  
+  float getPositionX();
+  float getPositionY();
+  
+private:
+  float x, y;
+}
+
+#endif // PLAYER_H
