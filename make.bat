d16784e23c0839d1b65668e48566410c2c87489e
Charlie Bouthoorn <c_bouthoorn@icloud.com>
2016-01-23 14:32:16 +0100

    Windows = Problems
    Trying to make this bitch work on Windows

--- make.bat
+++ make.bat
@@ -1,6 +1,27 @@
 @echo off
 
-"../../MinGW/bin/g++" -Wall  -I ./SFML/include  -I .   -c Item.hpp Item.cpp
-"../../MinGW/bin/g++" -Wall  -I ./SFML/include  -I .   -c Grid.hpp Grid.cpp
-"../../MinGW/bin/g++" -Wall  -I ./SFML/include  -I .   -c main.cpp
-"../../MinGW/bin/g++" -Wall  -I ./SFML/include  -I .    main.o -o main  -L ./SFML/lib -lsfml-audio  -lsfml-graphics  -lsfml-network  -lsfml-system  -lsfml-window
+
+echo THIS DOES NOT WORK !!!
+
+
+
+set SFML=./SFML-Windows-MinGW
+set PATH=%PATH%;C:\MinGW\bin;%SFML%;%SFML%/lib;%SFML%/bin;%SFML%/include
+
+echo Pre-compile Circle
+g++ -Wall  -c Circle.hpp Circle.cpp   -I %SFML%/include
+
+echo Pre-compile Item
+g++ -Wall  -c Item.hpp Item.cpp  -I %SFML%/include
+
+echo Pre-compile Grid
+g++ -Wall  -c Grid.hpp Grid.cpp -I %SFML%/include
+
+echo Pre-compile Player
+g++ -Wall  -c Player.hpp Player.cpp -I %SFML%/include
+
+echo Pre-compile main
+g++ -Wall  -c main.cpp  -I %SFML%/include
+
+echo Total compile
+g++ -Wall  -o main main.o  -I %SFML%/include -L %SFML%/lib -lsfml-main -lsfml-audio  -lsfml-graphics  -lsfml-network  -lsfml-system  -lsfml-window
